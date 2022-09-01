8e718b59876e3dcf0e069fe4433d51ceef87398e
Mau <87596299+Maucr27@users.noreply.github.com>
2022-09-01 16:16:43 -0600

    Add files via upload

---
+++ Ejerficios.py
@@ -0,0 +1,25 @@
+'''#Ejercicio 1
+print((3+2)/(2*5)**2)'''
+
+'''#Ejercicio 2
+horas=float(input("ingrese la cantidad de horas: "))
+costoHrs=float(input("Ingrese el costo por horas: "))
+pago=horas*costoHrs
+
+print("El precio a cobrar es de:",pago)'''
+
+'''#Ejercicio 3
+peso=float(input("Ingrese su peso: ",))
+altura=float(input("Ingrese su altura: ",))
+mc= (peso)/(altura*altura)
+print("su IMC es de: ",mc)'''
+
+'''#ejercicio 4
+trompo = 30
+yoyo = 20
+cat = int(input("Digite la cantidad de trompos que compro: "))
+cay = int(input("Digite la cantidad de yoyos que compro: "))
+pt = cat*trompo
+py = cay*yoyo
+ppaq = pt+py
+print("El peso total del paquete equivale a: ", ppaq, "Kg")'''
